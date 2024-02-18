74f687ef24c22d14eb3b0f637b5ee14b3ea6b15e
ChukwukaUbani <141528245+ChukwukaUbani@users.noreply.github.com>
2024-02-18 12:47:48 +0100

    util.py

--- util.py
+++ util.py
@@ -1,6 +1,22 @@
 from sqlalchemy import create_engine
+import psycopg2
 from dotenv import dotenv_values
 dotenv_values()
+
+
+config = dict(dotenv_values('.env'))
+
+def get_redshift_connection(config):
+    iam_role = config.get('IAM_ROLE')
+    user = config.get('USER')
+    password = config.get('PASSWORD')
+    host = config.get('HOST')
+    database_name = config.get('DATABASE_NAME')
+    port = config.get('PORT')
+    # create redshift connection usingf'dbname={database_name} host={host} port={port} user={user} password={password}')
+    conn = psycopg2.connect(f'postgresql://{user}:{password}@{host}:{port}/{database_name}')
+    return conn
+
 
 def get_database_conn():
     config = dict(dotenv_values('.env'))
